4d4da5d7a4b44753b8a6c32016ed3168205907c7
germanusK <germanuskeming@gmail.com>
2022-02-28 19:41:36 +0100

    First project push

--- src/main/java/Os2/backend/service/RecurrentService.java
+++ src/main/java/Os2/backend/service/RecurrentService.java
@@ -1,5 +1,28 @@
 package Os2.backend.service;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import Os2.backend.model.Package;
+import Os2.backend.model.Subscriber;
+import Os2.backend.controller.*;
+
+@EnableScheduling
 public class RecurrentService {
 
+	private String interval;
+	Package _package;
+	Subscriber subscriber;
+	
+	
+	
+	public  RecurrentService(SubscriberController.SubscriptionRequestModel subscription) {
+		this.interval = subscription.s_package.duration;
+	}
+	
+	@Scheduled(cron = "@hourly")
+	
+	public void scheduleSubscription(SubscriberController.SubscriptionRequestModel subscription) {
+		
+	}
 }
